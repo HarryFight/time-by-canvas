f9b13b02f6c456153b338f591be0fcda180ada77
xiangyi <xiangyifight@gmail.com>
2014-09-28 19:54:08 +0800

    修改为时间动画

--- time.js
+++ time.js
@@ -7,11 +7,10 @@ var RADIUS = 7;
 var DIGIT_WIDTH =(7+1)*2*(RADIUS+1);//每个数字所占的宽度（数组长度为7）
 var COLON_WIDTH = (4+1)*2*(RADIUS+1);//冒号宽度
 
-var endTime = new Date(2014,7,4,12,12,12,12);//截止日期
-var curShowTimeSeconds = 0;
-
 var balls = [];
 var colors = ["#70B5FF","#FFF64A","#A8FF75","#FF7070","#F5FF70","#407AB8","#835FFF","#BA54FF","#FF4EC3","#FF443E"];
+var curShowTime;//当前时间对象
+var count = 0;//时间闪烁（1秒20次）
 
 window.onload = function() {
 
@@ -21,25 +20,65 @@ window.onload = function() {
     //设定画布大小
 	canvas.width = WINDOW_WIDTH;
 	canvas.height = WINDOW_HEIGHT;
-    curShowTimeSeconds = getCurShowTimeSeconds();
+    curShowTime = getCurShowTime(); // 取得当前时间组合对象
 
     //1秒20帧绘制
     setInterval(function () {
         render(context);
         update();
+        count++
     },50);
 
 };
+/**
+ * 渲染操作
+ * @param cxt
+ */
+function render (cxt) {
+    var hours = curShowTime.hours;
+    var minutes = curShowTime.minutes;
+    var seconds = curShowTime.seconds;
+
+    //清除指定区域画布画布
+    cxt.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+
+    renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),cxt);
+    renderDigit(MARGIN_LEFT+DIGIT_WIDTH,MARGIN_TOP,parseInt(hours%10),cxt);
+
+    renderDigit(MARGIN_LEFT+2*DIGIT_WIDTH+COLON_WIDTH,MARGIN_TOP,parseInt(minutes/10),cxt);
+    renderDigit(MARGIN_LEFT+3*DIGIT_WIDTH+COLON_WIDTH,MARGIN_TOP,parseInt(minutes%10),cxt);
+
+    renderDigit(MARGIN_LEFT+4*DIGIT_WIDTH+2*COLON_WIDTH,MARGIN_TOP,parseInt(seconds/10),cxt);
+    renderDigit(MARGIN_LEFT+5*DIGIT_WIDTH+2*COLON_WIDTH,MARGIN_TOP,parseInt(seconds%10),cxt);
+    if(count>20){
+        renderDigit(MARGIN_LEFT+2*DIGIT_WIDTH,MARGIN_TOP,10,cxt);
+        renderDigit(MARGIN_LEFT+4*DIGIT_WIDTH+COLON_WIDTH,MARGIN_TOP,10,cxt);
+        if(count > 40){
+            count = 0;  //重置count
+        }
+    }
+
+
+    //遍历绘制小球
+    for(var i = 0;i < balls.length;i++){
+        cxt.fillStyle = balls[i].color;
+        cxt.beginPath();
+        cxt.arc( balls[i].x , balls[i].y , RADIUS , 0 , 2*Math.PI );
+        cxt.closePath();
+
+        cxt.fill();
+    }
+}
 
 function update() {
-    var nextShowTimeSeconds = getCurShowTimeSeconds();
-    var nextHours = parseInt(nextShowTimeSeconds/3600);
-    var nextMinutes = parseInt((nextShowTimeSeconds - nextHours*3600)/60);
-    var nextSeconds = nextShowTimeSeconds%60;
+    var nextShowTime = getCurShowTime();    //取得现在的时间组合对象
+    var nextHours = nextShowTime.hours;
+    var nextMinutes = nextShowTime.minutes;
+    var nextSeconds = nextShowTime.seconds;
 
-    var curHours = parseInt(curShowTimeSeconds/3600);
-    var curMinutes = parseInt((curShowTimeSeconds - nextHours*3600)/60);
-    var curSeconds = curShowTimeSeconds%60;
+    var curHours = curShowTime.hours;
+    var curMinutes = curShowTime.minutes;
+    var curSeconds = curShowTime.seconds;
 
     //判断时间是否发生了变化
     if(nextSeconds != curSeconds){
@@ -64,44 +103,11 @@ function update() {
         if(parseInt(nextSeconds%10) != parseInt(curSeconds%10)){
             addBall(MARGIN_LEFT+5*DIGIT_WIDTH+2*COLON_WIDTH,MARGIN_TOP,parseInt(curSeconds%10))
         }
-        //更新是时间值
-        curShowTimeSeconds = nextShowTimeSeconds;
+        //更新是时间对象
+        curShowTime = nextShowTime;
     }
     //更新小球运动参数
     updateBalls();
-}
-/**
- * 渲染操作
- * @param cxt
- */
-function render (cxt) {
-	// body...
-	var hours = parseInt(curShowTimeSeconds/3600);
-	var minutes = parseInt((curShowTimeSeconds - hours*3600)/60);
-//	var seconds = parseInt(curShowTimeSeconds - hours*3600 - minutes*60);
-    var seconds = curShowTimeSeconds%60;
-
-    //清除指定区域画布画布
-    cxt.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
-
-	renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),cxt);
-    renderDigit(MARGIN_LEFT+DIGIT_WIDTH,MARGIN_TOP,parseInt(hours%10),cxt);
-    renderDigit(MARGIN_LEFT+2*DIGIT_WIDTH,MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT+2*DIGIT_WIDTH+COLON_WIDTH,MARGIN_TOP,parseInt(minutes/10),cxt);
-    renderDigit(MARGIN_LEFT+3*DIGIT_WIDTH+COLON_WIDTH,MARGIN_TOP,parseInt(minutes%10),cxt);
-    renderDigit(MARGIN_LEFT+4*DIGIT_WIDTH+COLON_WIDTH,MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT+4*DIGIT_WIDTH+2*COLON_WIDTH,MARGIN_TOP,parseInt(seconds/10),cxt);
-    renderDigit(MARGIN_LEFT+5*DIGIT_WIDTH+2*COLON_WIDTH,MARGIN_TOP,parseInt(seconds%10),cxt);
-
-    //遍历绘制小球
-    for(var i = 0;i < balls.length;i++){
-        cxt.fillStyle = balls[i].color;
-        cxt.beginPath();
-        cxt.arc( balls[i].x , balls[i].y , RADIUS , 0 , 2*Math.PI );
-        cxt.closePath();
-
-        cxt.fill();
-    }
 }
 
 /**
@@ -175,13 +181,15 @@ function updateBalls(){
     }
 }
 /**
- * 返回现在所剩秒数
- * @returns {number}
+ * 返回当前时间组合的对象
+ * @returns {{}}
  */
-function getCurShowTimeSeconds(){
-    var curTime = new Date();
-    var ret = endTime.getTime() - curTime.getTime();
-    ret = Math.round(ret/1000);
+function getCurShowTime(){
+    var date = new Date(),
+        time = {};
+    time.hours = date.getHours();
+    time.minutes = date.getMinutes();
+    time.seconds = date.getSeconds();
 
-    return ret >= 0 ? ret : 0;
+    return time;
 }
